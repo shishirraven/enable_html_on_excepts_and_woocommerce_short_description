1561eeccf4d287ea6b46eaa81f30ed107cf11a7c
Shishir <shishir.raven@gmail.com>
2023-08-24 03:29:23 +0530

    first commit

---
+++ enable_html_on_excerpts.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: HTML on Excerpts
+Description: This plugin allows HTML in excerpts.
+Version: 1.0
+Author: Shishir Raven
+Author URI: https://www.webforttechnology.com/
+*/
+
+function filter_post_excerpt_attrs( $metadata ) {
+    if ( 'core/post-excerpt' === $metadata['name'] ) {
+        $metadata["attributes"]["excerptLength"] = false;
+    }
+
+    return $metadata;
+};
+
+add_filter( 'block_type_metadata', 'filter_post_excerpt_attrs', 10 );
